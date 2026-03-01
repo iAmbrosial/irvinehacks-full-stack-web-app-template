44657b32ca12bfd7c63c6b6a2c374137c217d23c
Bill Liu <bill.liu.797@gmail.com>
2026-03-01 08:38:25 -0800

    add render link to CORS

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://irvinehacks-formai.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
